373a43d9530a0bd8f1e216dd97072aea942dc64a
Jonathan Shovak <shovakitup@gmail.com>
2022-10-24 16:00:12 -0500

    started setting up object  factories and methods

---
+++ script.js
@@ -0,0 +1,27 @@
+
+//name marker
+const playerFactory = (name, marker) => {
+    // marker will mark a game-board spot with their marker...
+    const playerMove = () => ("marker")
+    return { name, marker, playerMove }; 
+}
+
+
+
+const gameBoard(() => {
+    const ;
+    const ;
+    return {
+        ,
+
+    };
+})();
+
+const displayController(() => {
+    const ;
+    const ;
+    return {
+        ,
+
+    };
+})();
